3ce43b1900880aed9780d88832482d7b0f2f464b
Araz Faraji <araz@Arazs-MacBook-Pro.local>
2020-01-10 14:36:43 -0500

    January 10th commit

---
+++ MERN/MERN Stack.tmp/React.tmp/hook_form/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import RegisterForm from './Components/Form';
+
+
+function App() {
+  return (
+    <div className="App">
+
+
+    
+      <RegisterForm />
+      {/* <TableofForm submittedInfo={[]}/> */}
+    </div>
+  );
+}
+
+export default App;
